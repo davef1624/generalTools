d047e9b14b65319f8e74e9da5ad1c77a72fac576
David Francheski <david.francheski@Davids-MacBook-Pro.local>
2025-02-26 22:05:02 -0800

    Configure Vault token

--- python-utils/hcVault.py
+++ python-utils/hcVault.py
@@ -2,7 +2,7 @@ import hvac
 
 def hcVaultClient():
     # Initialize the Vault client
-    client = hvac.Client(url='http://127.0.0.1:8200', token='my-root-token')
+    client = hvac.Client(url='http://127.0.0.1:8200', token='root')
 
     # Verify if the client is authenticated
     print(client.is_authenticated())  
